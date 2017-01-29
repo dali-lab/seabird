aa6e071d99eb28e8376d71bb017d65afaf183b16
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-01-29 18:29:03 -0500

    Added the Icons

--- Seabird/index.ios.js
+++ Seabird/index.ios.js
@@ -15,39 +15,130 @@ import {
 export default class Seabird extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+      }}>
+      <View style={styles.mainHeader}>
+        <TouchableHighlight>
+          <View style={styles.hamburgerMenu}></View>
+        </TouchableHighlight>
+      </View>
+      <TouchableHighlight onPress={this.navigate.bind(this, 'dds')}>
+        <View style={styles.mainSection}>
+        <Image
+          source={require('./Icons/Restaurant-50.png')}
+          style={styles.mainIcon}
+        />
+          <Text style={styles.mainLabel}>DDS HOURS</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <View style={styles.mainSection}>
+        <Image
+          source={require('./Icons/T-Shirt-50.png')}
+          style={styles.mainIcon}
+        />
+          <Text style={styles.mainLabel}>LAUNDRY</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <View style={styles.mainSection}>
+        <Image
+          source={require('./Icons/News-50.png')}
+          style={styles.mainIcon}
+        />
+          <Text style={styles.mainLabel}>NEWS</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <View style={styles.mainSection}>
+        <Image
+          source={require('./Icons/Map Marker-50.png')}
+          style={styles.mainIcon}
+        />
+          <Text style={styles.mainLabel}>CAMPUS NEWS</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <View style={styles.mainSection}>
+        <Image
+          source={require('./Icons/Calendar-50.png')}
+          style={styles.mainIcon}
+        />
+          <Text style={styles.mainLabel}>SCHEDULE</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <View style={styles.mainSection}>
+        <Image
+          source={require('./Icons/Sport-50.png')}
+          style={styles.mainIcon}
+        />
+          <Text style={styles.mainLabel}>SPORTS</Text>
+        </View>
+      </TouchableHighlight>
+      <View style={styles.nextButton}></View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  /* Style for the header section that holds the school name and crest */
+  mainHeader: {
+    width: 400,
+    height: 80,
+    marginTop: 20,
+    backgroundColor: 'white',
+  },
+
+  /* Style for the six buttons on the main page */
+  mainSection: {
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    width: 150,
+    height: 150,
+    borderRadius: 5,
+    paddingBottom: 20,
+    margin: 8,
+    backgroundColor: '#ccc',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+
+  /* Style for the bottom button that moves to the next page */
+  nextButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
     margin: 10,
+    marginTop: 5,
+    backgroundColor: '#99ff66',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  /* Style for the main labels on the main buttons */
+  mainLabel: {
+    top: 30,
+    fontSize: 18,
+    fontFamily: 'Avenir Next',
+    fontWeight: '500',
   },
+
+  /* Style for the icons on the main buttons */
+  mainIcon: {
+    top: 10,
+  },
+
+  /* Style for the hamburgerMenu */
+  hamburgerMenu: {
+    width: 40,
+    height: 40,
+    left: 30,
+    top: 10,
+    backgroundColor: 'black',
+  }
 });
 
 AppRegistry.registerComponent('Seabird', () => Seabird);
