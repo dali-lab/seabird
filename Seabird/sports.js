d106ed5c0a0df7b6c73c5ffb6bad68a79e668ed6
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-02-05 11:28:13 -0500

    Included navigation system on the Sports page

--- Seabird/sports.js
+++ Seabird/sports.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Image, Animated, StyleSheet, LinkingIOS, ScrollView, ListView, View, Text, Navigator, AppRegistry, PropTypes, TouchableHighlight, WebView } from 'react-native';
+import { Image, Animated, StyleSheet, LinkingIOS, ScrollView, ListView, View, Text, Navigator, AppRegistry, PropTypes, TouchableHighlight, WebView, TouchableOpacity } from 'react-native';
 
-
+var WEBVIEW_REF = 'webview';
 
 export default class Sports extends Component {
   // Initialize the hardcoded data
@@ -13,14 +13,91 @@ export default class Sports extends Component {
     }
   };
 
+  onNavigationStateChange(navState) {
+    this.setState({
+      canGoBack: navState.canGoBack
+    });
+  };
+
+  onBack() {
+    this.refs[WEBVIEW_REF].goBack();
+  };
+
+  navigate(routeName) {
+    this.props.navigator.pop();
+  };
+
   render() {
     return (
+      <View style={styles.container}>
+      <View style={styles.mainHeader}>
+      <TouchableHighlight onPress={this.navigate.bind(this, 'dds')}>
+        <Image
+          source={require('./Icons/Restaurant-50.png')}
+          style={styles.mainIcon}
+        />
+        </TouchableHighlight>
+        <Text style={styles.schoolTitle}>Dartmouth College</Text>
+        <Image
+          source={require('./Icons/Restaurant-50.png')}
+          style={styles.mainIcon}
+        />
+      </View>
       <WebView
-        source={{uri: 'http://www.dartmouthsports.com'}}
-        style={{marginTop: 20}}
+      ref={WEBVIEW_REF}
+      style={{flex: 1}}
+      onNavigationStateChange=
+        {this.onNavigationStateChange.bind(this)}
+      source={{uri: 'http://dartmouthsports.com'}}
       />
+      <View style={styles.bottomBar}>
+        <TouchableOpacity
+          disabled={!this.state.canGoBack}
+          onPress={this.onBack.bind(this)}
+          >
+          <Text style={styles.bottomBarText}>Go Back</Text>
+        </TouchableOpacity>
+      </View>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  /* Styles the entire container to hold multiple views */
+  container: {
+      flex: 1,
+    },
+
+  /* Style for the header section that holds the school name and crest */
+  mainHeader: {
+    width: 400,
+    height: 70,
+    backgroundColor: '#2b2b2b',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  /* Style for the school title text */
+  schoolTitle: {
+    fontSize: 23,
+    fontFamily: 'System',
+    fontWeight: '300',
+    color: '#fff',
+    letterSpacing: -0.56,
+  },
+
+  /* Styles the bottom web view navigation banner */
+  bottomBar: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 50,
+    backgroundColor: '#2b2b2b',
+  }
+});
 
 AppRegistry.registerComponent('Sports', () => Sports);
