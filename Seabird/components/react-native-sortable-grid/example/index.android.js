31978626bc505761f0c535f00268ef52fb652436
cannse <sean.m.cann.17@dartmouth.edu>
2017-05-11 21:06:08 -0400

    SortableGrid component separated from node module, customize portal drag-scrolling is still not stable

---
+++ Seabird/components/react-native-sortable-grid/example/index.android.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native'
+// import example from './basicExample.js'
+import example from './deletionExample.js'
+
+AppRegistry.registerComponent('example', () => example);
