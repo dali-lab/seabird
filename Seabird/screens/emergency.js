06bc9281d159a94cef424806d4b199982f6f7e9e
Tyler Justin Fisher <tylerjustinfisher@gmail.com>
2017-05-14 07:04:41 -0400

    phone call, email, & text functionality added

--- Seabird/screens/emergency.js
+++ Seabird/screens/emergency.js
@@ -20,6 +20,7 @@ import {
 import { NavBar } from './../components/navBar';
 import Firebase from '../firebase/firebase';
 import Database from '../firebase/database';
+import Communications from 'react-native-communications';
 
 var firebase = require("firebase/app");
 require("firebase/auth");
@@ -63,9 +64,38 @@ export default class Emergency extends Component {
         />
         <View style={styles.mainContent}>
         <ScrollView ref={(scrollView) => { _scrollView = scrollView; }}>
+          <Text>All texts after this one are *functional*, clickable buttons</Text>
+          <Text>~~~</Text>
+
           <TouchableHighlight onPress={() => Linking.openURL('https://dali.dartmouth.edu')}>
               <Text>Open the DALI website externally</Text>
           </TouchableHighlight>
+
+          <Text>~~~</Text>
+
+          <TouchableHighlight onPress={() => Communications.web('https://dali.dartmouth.edu')}>
+              <Text>Another way to open the site externally</Text>
+          </TouchableHighlight>
+
+          <Text>~~~</Text>
+
+          <TouchableHighlight onPress={() => Communications.phonecall('6316554692', true)}>
+              <Text>This one makes a phonecall, but only works on device</Text>
+          </TouchableHighlight>
+
+          <Text>~~~</Text>
+
+          <TouchableHighlight onPress={() => Communications.textWithoutEncoding('5555648583', "hey")}>
+              <Text>Send a text to "Kate" (on simulator, not device)</Text>
+          </TouchableHighlight>
+
+          <Text>~~~</Text>
+
+          <TouchableHighlight onPress={() => Communications.email(null, null, null, null, null)}>
+              <Text>Send an email (again, only on device)</Text>
+          </TouchableHighlight>
+
+          <Text>~~~</Text>
         </ScrollView>
         </View>
       </View>
