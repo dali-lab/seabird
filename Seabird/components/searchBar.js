d685d9808013c373087c6f8e35a8d178a6a56e1f
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-05-23 23:30:37 -0400

    Including the searchBar.js changes

---
+++ Seabird/components/searchBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  View,
+  TouchableHighlight,
+  Image,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+
+export class SearchBar extends Component {
+
+  navigatePop() {
+    this.props.navigator.pop();
+  }
+
+  render() {
+    return (
+      <View style={this.props.searchSectionStyle}>
+        <TextInput
+          style={this.props.searchInputStyle}
+          placeholder="Search Modules"
+          placeholderTextColor='rgba(255, 255, 255, 0.8)'
+          selectionColor="white"
+          onChangeText={(text) => {
+            this.props.onChangeText(text);
+          }}
+        />
+        <TouchableHighlight
+          ref="SearchBar"
+          underlayColor="transparent"
+          style={this.props.searchButtonStyle}
+          onPress={() => console.log('Pressed search')}
+        >
+          <Image
+            style={this.props.searchIconStyle} source={require('./../Icons/search_icon.png')}
+            />
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
